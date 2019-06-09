6d6a35094c1157cb19eb2c4c836b83e4a216b033
mark sauer-utley <mark.utley.713@gmail.com>
2019-06-08 20:55:06 -0400

    edit head tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,16 @@ export default class MyDocument extends Document {
 	render() {
 		return (
 			<html>
-				<Head>{this.props.styleTags}</Head>
+				<Head>
+					<link
+						rel="shortcut icon"
+						type="image/x-icon"
+						href="/static/favicon.ico"
+					/>
+					<title>mark sauer-utley</title>
+					<meta name="keywords" content="mark sauer-utley,developer,software,engineer,full-stack,frontend,front-end,javascript,js,next.js,gatsby,react,node.js,node,new york,nyc,new york city,brooklyn,web development"></meta>
+					{this.props.styleTags}
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
